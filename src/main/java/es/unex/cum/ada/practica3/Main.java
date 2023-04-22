989f5a2c54741033e7d59094bfb09e2cd95aeacb
Pablo Setrakian Bearzotti <pablosetra@icloud.com>
2023-04-22 18:16:01 +0200

    11/12

--- src/main/java/es/unex/cum/ada/practica3/Main.java
+++ src/main/java/es/unex/cum/ada/practica3/Main.java
@@ -2,49 +2,37 @@ package main.java.es.unex.cum.ada.practica3;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     private static class Section implements Comparable<Section> {
 
-        int position;
+        int middlePoint;
         int length;
-        int startPoint;
-        int endPoint;
-        boolean takeIt;
+        boolean valid;
 
-        public Section(int position, int length) {
-            this.position = position;
+        public Section(int middlePoint, int length) {
+            this.middlePoint = middlePoint;
             this.length = length;
         }
 
-        public int getPosition() {
-            return position;
+        public int getMiddlePoint() {
+            return middlePoint;
         }
 
         public int getLength() {
             return length;
         }
 
-        public void setStartPoint(int startPoint) {
-            this.startPoint = startPoint;
-        }
-
-        public void setEndPoint(int endPoint) {
-            this.endPoint = endPoint;
-        }
-
-        public int getStartPoint() {
-            return startPoint;
-        }
-
-        public int getEndPoint() {
-            return endPoint;
-        }
-
-        public void takeIt(boolean takeIt) {
-            this.takeIt = takeIt;
+        public void setValid(boolean valid) {
+            this.valid = valid;
         }
 
         @Override
@@ -52,55 +40,116 @@ public class Main {
             return Integer.compare(section.length, this.length);
         }
 
+        public boolean intersects(Section section) {
+            int halfLength = length / 2;
+            int otherHalfLength = section.getLength() / 2;
+    
+           
+            int start = middlePoint - halfLength;
+            int end = middlePoint + halfLength;
+    
+          
+            int otherStart = section.middlePoint - otherHalfLength;
+            int otherEnd = section.middlePoint + otherHalfLength;
+    
+            return (start <= otherEnd && otherStart <= end);
+        }
+
         @Override
         public String toString() {
-            return "Section [position=" + position + ", length=" + length + ", startPoint=" + startPoint + ", endPoint="
-                    + endPoint + ", takeIt=" + takeIt + "]";
+            return "Section [middlePoint=" + middlePoint + ", length=" + length + ", valid=" + valid + "]";
         }
 
-        public boolean isTakeIt() {
-            return takeIt;
+        public boolean isValid() {
+            return valid;
         }
+
+        public boolean coversStartPoint(){
+            boolean coversStartPoint = false;
+            if (middlePoint - (length/2) <= 0) {
+                coversStartPoint = true; 
+            }
+            return coversStartPoint;
+        }
+        
+        public boolean coversEndPoint(int endPoint){
+            boolean coversEndPoint = false;
+            if (middlePoint + (length/2) >= endPoint ) {
+                coversEndPoint = true; 
+            }
+            return coversEndPoint;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Section other = (Section) obj;
+            if (middlePoint != other.middlePoint)
+                return false;
+            if (length != other.length)
+                return false;
+            return true;
+        }
+
     }
+
 
     private static List<Section> sections = new ArrayList<>();
-
-    public static boolean coversStartPoint(Section section){
-        boolean coversStartPoint = false;
-        if (section.getStartPoint() <= 0) {
-            coversStartPoint = true; 
-        }
-        return coversStartPoint;
-    }
-
-    public static boolean coversEndPoint(Section section, int endPoint){
-        boolean coversEndPoint = false;
-        if (section.getEndPoint() >= endPoint ) {
-            coversEndPoint = true; 
-        }
-        return coversEndPoint;
-    }
-
+    
 
     public static int minSectionsRequired(List<Section> sections, int routeLength) {
         boolean startCovered = false;
         boolean endCovered = false;
-        int count = 0;
-        //si los tramos no son iguales
-        sections.get(0).takeIt(true);
-        for (int i = 1; i < sections.size(); i++) {
-            if (coversStartPoint(sections.get(i)) && !startCovered) {
-                sections.get(i).takeIt(true);
-                startCovered = true;
+        //la utilizare de auxiliar para ir viendo si los segmentos son contiguos
+        Section auxSection = null;
 
+        int numOfSections = -1;
+
+        //compruebo si la ruta cubre al menos el principio y el final
+        for (Section section : sections) {
+            if (section.coversStartPoint() && !startCovered) {
+                section.setValid(true);
+                startCovered = true;
+                auxSection = section;
             }
-            if (coversEndPoint(sections.get(i), routeLength) && !endCovered) {
-                sections.get(i).takeIt(true);
+
+            if (section.coversEndPoint(routeLength) && !endCovered) {
                 endCovered = true;
             }
         }
-        
-        return count;
+        //si no cubre el principio y el final no hay nada que hacer
+        if (!startCovered || !endCovered) {
+            return numOfSections;
+        }
+        //si el segmento del principio cubre el final solamente hay 1 segmento
+        else if (auxSection.coversEndPoint(routeLength)) {
+            return 1;
+        }
+        //si hay mas segmentos
+        else{
+            //tomo el del principio
+            numOfSections = 1;
+            for (Section section : sections) {
+                //empiezo a comparar utilizando el segmento del principio con el mas largo (no con el mismo) 
+                if (auxSection.intersects(section) && !auxSection.equals(section) ) {
+
+                    auxSection = section;
+
+                    numOfSections++;
+                    
+                    // si se tocan lo pillo y si ademas resulta que justo ese llega hasta el final devuelvo el numero de segmentos que llevo
+                    if (section.coversEndPoint(routeLength)) {
+                        return numOfSections;
+                    }
+                }
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
@@ -111,8 +160,6 @@ public class Main {
 
         for (int i = 0; i < numOfSections; i++) {
             Section section = new Section(scanner.nextInt(), scanner.nextInt());
-            section.setStartPoint(section.getPosition() - (section.getLength() / 2));
-            section.setEndPoint(section.getPosition() + (section.getLength() / 2));
             sections.add(section);
         }
 
@@ -121,6 +168,5 @@ public class Main {
         Collections.sort(sections);
 
         System.out.println(minSectionsRequired(sections, routeLenght));
-
     }
 }
